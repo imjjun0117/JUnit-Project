b4b6ff979bcbf82e06a3bb35bd3417aa28ed3740
황성준 <imjjun0117@naver.com>
2022-11-21 23:16:17 +0900

    테스í단위 테스트 작성

--- src/test/java/site/joony/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/joony/junitproject/domain/BookRepositoryTest.java
@@ -1,28 +1,118 @@
 package site.joony.junitproject.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩 (단위 테스트)
 public class BookRepositoryTest {
-
+    //통합 테스트 실행할 경우 메서드들은 순차적으로 실행되지 않는다. --> 순서 보장을 하고 싶다면 Order어
     @Autowired // DI
     private BookRepository bookRepository;
 
-    //1. 책 등록
+// primary key auto_increment 값이 초기화가 안됨 --> error 발생 아래와 같은 방법으로 해결되지만 속도가 느림
+//    @Autowired
+//    private EntityManager em;
+//
+//    @AfterEach
+//    public void db_init(){
+//        bookRepository.deleteAll();
+//        em.createNativeQuery("ALTER TABLEbook ALTER COLUMN id RESTART WITH 1")
+//                .executeUpdate();
+//    }
 
+
+    //    @BeforeAll // 테스트 시작 전에 한번만 실행
+    @BeforeEach // 각 테스트 시작 전에 한번씩 실행
+    public void 데이터준비(){
+
+        String title = "junit";
+        String author = "hwang";
+        Book book = Book.builder().title(title).author(author).build();
+
+        Book bookPS = bookRepository.save(book);
+    }//트랜잭션 종료가 안된다
+    //그렇다면 트랜잭션이 언제까지 유지될까?
+    //
+    // 가정 1. [ 데이터준비() + 1 책등록 ](Transaction1) , [ 데이터준비() + 2 책목록보기 ](Transaction2) (검증 완료)
+    // 가정 2. [ 데이터준비() + 1 책등록 + 데이터준비() + 2 책등록보기 ](Transaction1) (검증 실패)
+
+    //1. 책 등록
     @Test
     public void 책등록_test(){
-        System.out.println("책등록_test 실행");
-    }
+        // 1. given ( 데이터 준비 )
+        String title = "junit5";
+        String author = "joony";
+        Book book = Book.builder().title(title).author(author).build();
+
+        // 2. when ( 테스트 실행 )
+        Book bookPS = bookRepository.save(book);
+
+        //3. then ( 검증 )
+        Assertions.assertEquals(title, bookPS.getTitle());
+        Assertions.assertEquals(author, bookPS.getAuthor());
+
+    }//트랜잭션 종료(저장된 데이터 초기화)
 
     //2. 책 목록보기
+    @Test
+    public void 책목록보기_test(){
+        //given
+        String title = "junit";
+        String author = "hwang";
+
+        //when
+        List<Book> booksPS = bookRepository.findAll();
+
+        System.out.println("트랜잭션 검증 : =======================> " +booksPS.size());
+
+        //then
+        Assertions.assertEquals(title, booksPS.get(0).getTitle());
+        Assertions.assertEquals(author, booksPS.get(0).getAuthor());
+
+    }//트랜잭션 종료(저장된 데이터 초기화)
+
 
     //3. 책 한권보기
+    @Sql("classpath:db/tableInit.sql") // book 엔터티를 초기화하고 테스트 실행 (id를 찾는 모든 테스트에 붙여주는게 좋음)
+    @Test
+    public void 책한권보기_test(){
+        //given
+        String title = "junit";
+        String author = "hwang";
 
-    //4. 책 수정
+        //when
+        Book bookPS = bookRepository.findById(1L).get();
 
-    //5. 책 삭제
+        //then
+        Assertions.assertEquals(title, bookPS.getTitle());
+        Assertions.assertEquals(author, bookPS.getAuthor());
+
+    }//트랜잭션 종료(저장된 데이터 초기화)
+
+    //4. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책삭제_test(){
+        //given
+        Long id = 1L;
+
+        //when
+        bookRepository.deleteById(id);
+
+        //then
+        Assertions.assertFalse(bookRepository.findById(id).isPresent()); // assertFlase -> false일 경우 성공
+    }
+
+
+    //5. 책 수정
+
+
+
 
 }
